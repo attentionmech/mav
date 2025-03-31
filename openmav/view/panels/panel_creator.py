b853be3798f44efe7e556d7b975a657e73e28ebd
attentionmech <attentionmech@gmail.com>
2025-03-31 20:20:52 +0530

    refactor panels to use measurement api

--- openmav/view/panels/panel_creator.py
+++ openmav/view/panels/panel_creator.py
@@ -1,56 +1,32 @@
-from rich.panel import Panel
-
 from openmav.api.measurements import ModelMeasurements
-from openmav.view.panels.panel_provider import PanelProvider
+from openmav.view.panels.internal_panels import (AttentionEntropyPanel,
+                                                 GeneratedTextPanel,
+                                                 MLPActivationsPanel,
+                                                 OutputDistributionPanel,
+                                                 TopPredictionsPanel)
 
 
 class PanelCreator:
+    def __init__(self, max_bar_length=20, limit_chars=50, num_bins=20):
+        self.max_bar_length = max_bar_length
+        self.limit_chars = limit_chars
+        self.num_bins = num_bins
 
-    def __init__(self, max_bar_length=20, limit_chars=50):
-        self.panel_provider = PanelProvider(
-            max_bar_length=max_bar_length, limit_chars=limit_chars
-        )
-
-    def get_panels(self, measurement: ModelMeasurements):
-
-        panel_definitions = {
-            "top_predictions": Panel(
-                self.panel_provider.create_top_predictions_panel_content(
-                    measurement.decoded_tokens,
-                    measurement.top_ids,
-                    measurement.top_probs,
-                    measurement.logits,
-                ),
-                title="Top Predictions",
-                border_style="blue",
-            ),
-            "mlp_activations": Panel(
-                self.panel_provider.create_activations_panel_content(
-                    measurement.mlp_normalized,
-                    measurement.mlp_activations,
-                ),
-                title="MLP Activations",
-                border_style="cyan",
-            ),
-            "attention_entropy": Panel(
-                self.panel_provider.create_entropy_panel_content(
-                    measurement.entropy_values,
-                    measurement.entropy_normalized,
-                ),
-                title="Attention Entropy",
-                border_style="magenta",
-            ),
-            "output_distribution": Panel(
-                self.panel_provider.create_prob_bin_panel(measurement.next_token_probs),
-                title="Output Distribution",
-                border_style="yellow",
-            ),
-            "generated_text": Panel(
-                self.panel_provider.create_generated_text_panel(
-                    measurement.generated_text, measurement.predicted_char
-                ),
-                title="Generated text",
-                border_style="green",
-            ),
+    def get_panels(self, measurements: ModelMeasurements):
+        return {
+            "top_predictions": TopPredictionsPanel(
+                measurements, self.max_bar_length, self.limit_chars
+            ).get_panel(),
+            "mlp_activations": MLPActivationsPanel(
+                measurements, self.max_bar_length, self.limit_chars
+            ).get_panel(),
+            "attention_entropy": AttentionEntropyPanel(
+                measurements, self.max_bar_length, self.limit_chars
+            ).get_panel(),
+            "output_distribution": OutputDistributionPanel(
+                measurements, self.max_bar_length, self.num_bins
+            ).get_panel(),
+            "generated_text": GeneratedTextPanel(
+                measurements, self.max_bar_length, self.limit_chars
+            ).get_panel(),
         }
-        return panel_definitions
