006a0c083b61b0eb8eac9a88a1f87debfb01ba52
attentionmech <attentionmech@gmail.com>
2025-03-31 23:15:19 +0530

    external plugins test

--- openmav/view/panels/panel_creator.py
+++ openmav/view/panels/panel_creator.py
@@ -3,12 +3,11 @@ import re
 from typing import List, Optional
 
 from openmav.api.measurements import ModelMeasurements
-from openmav.view.panels import internal_panels  # Import the entire module
+from openmav.view.panels import internal_panels
 from openmav.view.panels.panel_base import PanelBase
 
 
 def capital_to_snake(text):
-
     result = [text[0].lower()]
     for char in text[1:]:
         if char.isupper():
@@ -32,25 +31,36 @@ class PanelCreator:
         self.limit_chars = limit_chars
         self.num_bins = num_bins
         self.selected_panels = selected_panels
-        self.external_panels = external_panels
+        self.external_panels = external_panels or []  # Ensure external_panels is never None
 
     def get_panels(self, measurements: ModelMeasurements):
-        # Get all classes in `internal_panels` that inherit from `PanelBase`
-
-        panel_classes = {
+        # Get internal panel classes (still removing "Panel" suffix)
+        internal_panel_classes = {
             capital_to_snake(name[: -len("Panel")]): cls
             for name, cls in inspect.getmembers(internal_panels, inspect.isclass)
             if issubclass(cls, PanelBase) and cls is not PanelBase
         }
 
-        # import pdb; pdb.set_trace()
+        # Get external panel classes (using full name without "Panel" removal)
+        external_panel_classes = {}
+        if self.external_panels:
+            for panel in self.external_panels:
+                if isinstance(panel, type) and issubclass(panel, PanelBase):
+                    #don't want to make any effort on external panel naming convention..
+                    panel_name = panel.__name__
+                    external_panel_classes[panel_name] = panel
+                elif isinstance(panel, PanelBase):
+                    # If it's an instance, get its class full name
+                    panel_name = capital_to_snake(panel.__class__.__name__)
+                    external_panel_classes[panel_name] = panel.__class__
 
-        # Create panel instances dynamically
+        all_panel_classes = {**internal_panel_classes, **external_panel_classes}
+
         panel_definitions = {
             name: panel_cls(
                 measurements, self.max_bar_length, self.limit_chars
             ).get_panel()
-            for name, panel_cls in panel_classes.items()
+            for name, panel_cls in all_panel_classes.items()
         }
 
         if self.selected_panels is None:
@@ -63,7 +73,6 @@ class PanelCreator:
         ]
 
         if not panels:
-            # print exception that no valid panels are provided
             raise ValueError("No valid panels provided")
 
         return panels
